d1608edcc35b8ccf2c3ec69cc11e0ea0735fe66d
ArthurBondar <arthur.bondar.1@gmail.com>
2018-02-08 09:24:54 -0400

    Feb 8, 2018

---
+++ RPI/DataBase.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+
+#define FILE_PATH "DataBase"
+#define SMFR_PATH "Semaphore"
+
--- RPI/Simulator.c
+++ RPI/Simulator.c
@@ -3,36 +3,73 @@
 #include <string.h>
 //#include <asm/io.h>
 
+// Defenitions
 #define MAX_DATA 341796
 #define CREATE  50
 #define FILE_PATH "DataBase"
+#define SMFR_PATH "Semaphore"
+#define TIME_INTERVAL 30
 
+// Functions
+void updateDate (char* date);
+void fileWrite (char* path, char* data);
+
+// MAIN
 int main ()
 {
     char file_name[27];
-    int rec_num, sec = 10, min = 30, hour = 15;
-    char date[] = "2017_07_28";
-    FILE* file;
-    
-    for(rec_num = 0; rec_num < CREATE; rec_num++)
-    {
-        sprintf(file_name, "%s/%06d_%s_%02d:%02d:%02d.dat", FILE_PATH, rec_num, date, hour, min, sec);
-        file = fopen(file_name,"w");
-        fclose(file);
+    char date[] = "2017_07_28_15:30:23";
 
-        sec += 30;
-        if(sec >= 60){
-            sec -= 60;
-            min ++;
-        }
-        if(min >= 60){
-            min -= 60;
-            hour ++;
-        }
-        if(hour >= 24){
-            hour -= 24;
-        }
+    for(int rec_num = 0; rec_num < CREATE; rec_num++)
+    {
+        sprintf(file_name, "%s/%06d_%s.dat", FILE_PATH, rec_num, date);
+        fileWrite(file_name, "0");
+        updateDate(date);
     }
 
     return 0;
 }
+
+/*
+    Incrementing date-time and rewriting the string [date]
+*/
+void updateDate (char* date)
+{
+    int year, month, day, hour, min, sec;
+
+    sscanf(date, "%d_%d_%d_%d:%d:%d", &year, &month, &day, &hour, &min, &sec);
+    sec += TIME_INTERVAL;
+    if(sec >= 60){
+        sec -= 60;
+        min ++;
+    }
+    if(min >= 60){
+        min -= 60;
+        hour ++;
+    }
+    if(hour >= 24){
+        hour -= 24;
+        day ++;
+    }
+    if(day > 29) {
+        day -= 7;
+        month ++;
+    }
+    if(month >= 12){
+        month -= 12;
+        year++;
+    }
+    sprintf(date, "%04d_%02d_%02d_%02d:%02d:%02d", year, month, day, hour, min, sec);
+}
+
+/*
+    Writing [data] into a file specified by [path]
+*/
+void fileWrite (char* path, char* data)
+{
+    FILE* file = fopen(path,"w");
+
+
+    fclose(file);
+}
+
