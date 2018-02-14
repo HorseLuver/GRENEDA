eded1f712ca766e2a58db09f249d3c427b2d4434
ArthurBondar <arthur.bondar.1@gmail.com>
2018-02-14 15:18:59 -0400

    Feb 14, 2018. Ben is a wizard

--- RPI/Simulator.c
+++ RPI/Simulator.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
 //#include <asm/io.h>
 
 // Defenitions
 #define MAX_DATA 341796
-#define CREATE  50
+#define CREATE  5
 #define FILE_PATH "DataBase"
 #define SMFR_PATH "Semaphore"
 #define TIME_INTERVAL 30
@@ -17,13 +18,30 @@ void fileWrite (char* path, char* data);
 // MAIN
 int main ()
 {
-    char file_name[27];
+    char file_name[50], data[150], temp[50];
     char date[] = "2017_07_28_15:30:23";
+    float random;
 
+    srand(time(NULL));         // should only be called once
+
+    // Loop to create each record file
     for(int rec_num = 0; rec_num < CREATE; rec_num++)
     {
+        // Printing Date
+        strcpy(data, date);
+        // Printing sensor readings S1 S2 S3 ...     
+        for(int i = 0; i < 6; i++)      
+        {
+            random = (float)(rand() % 20) / 100;      
+            sprintf(temp, " %f", random);
+            strcat(data, temp);
+        }
+        //strcat(data, (const char*) '\n');
+        sprintf(data, "%s\n", data);
+        printf("Data: %s", data);
+        // Getting file Name
         sprintf(file_name, "%s/%06d_%s.dat", FILE_PATH, rec_num, date);
-        fileWrite(file_name, "0");
+        fileWrite(file_name, data);
         updateDate(date);
     }
 
@@ -68,8 +86,8 @@ void updateDate (char* date)
 void fileWrite (char* path, char* data)
 {
     FILE* file = fopen(path,"w");
-    
-    
+    //fprintf(file, "%s", data);
+    fputs(data, file);
     fclose(file);
 }
 
