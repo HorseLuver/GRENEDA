d42200e4d02a2d2fc2b2c1631145861d4d86b50d
Kyle MacIsaac <Kyle@Kyles-MacBook-Pro.local>
2018-03-04 19:06:54 -0400

    Image Handling Update - 4 March 18

---
+++ Imagey Stuff/Image_Grab.c
@@ -0,0 +1,30 @@
+/*
+Date: 4 March, 2018
+Image_Grab code used to copy images from the DataBase to the output
+This program will pull all the pictures from 1 day selected by user
+Copies all of the pictures to the Windmills!!! folder
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+
+//Declarations
+#define DB_PATH "DataBase"
+#define SMFR_PATH "Semaphore"
+
+#define Y1 0
+#define Y2 1
+#define Y3 2
+#define Y4 3
+#define M1 4
+#define M2 5
+#define D1 6
+#define D2 7
+
+void main()
+{
+    
+}
